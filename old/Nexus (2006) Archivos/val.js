1a6384a52ecd4b53f43525946313424cc15546bd
Eloyaso2008 <eloyaso2008@gmail.com>
2025-06-13 18:52:59 -0600

    Add files via upload

---
+++ old/Nexus (2006) Archivos/val.js
@@ -0,0 +1,85 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function imagen(){
+window.open('../upload/default.htm','popup','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=231,height=25');
+}
+
+function estatus(msg)
+{
+  window.status = msg
+  return true
+}
+
+function fullScreen() {
+window.open('contenidofs.asp', '', 'fullscreen=yes, scrollbars=yes');
+}
+function Resetting()
+
+{
+
+//Responds to onReset event.
+
+return window.confirm("Are you sure you want to clear everything?")
+
+}
+
+function Submitting()
+
+{
+
+//Responds to onSubmit event.
+
+return window.confirm("¿Esta seguro que desea borrar los datos?")
+
+}
+
+function help(){
+window.open('help.htm','new','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=500,height=400');
+}
+function msg(){
+alert('No hay archivo disponible');
+}
+
+
+
+function chat(){
+window.open('https://web.archive.org/web/20061208030018/http://148.234.9.16/aula/default.htm','popup','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=650,height=350');
+}
+function abrirvideo(){
+window.open('https://web.archive.org/web/20061208030018/http://148.234.9.16/video/video.htm','popup','toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=no,resizable=no,width=550,height=350');
+}
+
+
+
+
+}
+/*
+     FILE ARCHIVED ON 03:00:18 Dec 08, 2006 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 22:29:11 Apr 13, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.513
+  exclusion.robots: 0.021
+  exclusion.robots.policy: 0.011
+  esindex: 0.013
+  cdx.remote: 10.85
+  LoadShardBlock: 66.834 (3)
+  PetaboxLoader3.datanode: 110.24 (4)
+  load_resource: 329.113
+  PetaboxLoader3.resolve: 243.078
+*/
