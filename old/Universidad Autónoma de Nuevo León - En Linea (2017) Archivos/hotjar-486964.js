2b112421da852b9fc089154055569fb06063cc50
Eloyaso2008 <eloyaso2008@gmail.com>
2025-06-13 19:09:22 -0600

    Add files via upload

---
+++ old/Universidad Autónoma de Nuevo León - En Linea (2017) Archivos/hotjar-486964.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+window.hjSiteSettings = window.hjSiteSettings || {"record_targeting_rules":[],"surveys":[],"features":[],"feedback_widgets":[],"recording_capture_keystrokes":true,"polls":[],"site_id":486964,"integrations":{"optimizely":{"tag_recordings":false}},"testers_widgets":[],"forms":[],"record":false,"heatmaps":[],"deferred_page_contents":[],"rec_value":null,"r":0.0523732986,"state_change_listen_mode":"automatic"};
+
+window.hjBootstrap=window.hjBootstrap||function(d){var e=function(){},a=document,f=a.head||a.getElementsByTagName("head")[0],c,b;a.addEventListener&&(c=a.createElement("script"),c.async=1,c.src=d,f.appendChild(c),c=["iframe#_hjRemoteVarsFrame {","display: none !important; width: 1px !important; height: 1px !important; opacity: 0 !important; pointer-events: none !important;","}"],d=a.createElement("style"),d.type="text/css",d.styleSheet?d.styleSheet.cssText=c.join(""):d.appendChild(a.createTextNode(c.join(""))),
+f.appendChild(d),b=a.createElement("iframe"),b.style.cssText=c[1],b.name="_hjRemoteVarsFrame",b.title="_hjRemoteVarsFrame",b.id="_hjRemoteVarsFrame",b.src="https://"+(window._hjSettings.varsHost||"vars.hotjar.com")+"/rcj-99d43ead6bdf30da8ed5ffcb4f17100c.html",b.onload=function(){e.varsLoaded=!0;"undefined"!=typeof hj&&hj.event&&hj.event.signal("varsLoaded")},e.varsJar=b,a.body?a.body.appendChild(b):a.addEventListener("DOMContentLoaded",function(){a.body.appendChild(b)}),window.hjBootstrap=e)};
+
+
+hjBootstrap('https://web.archive.org/web/20171228053517/https://script.hotjar.com/modules-526d80f8c01454f84b75838f21c8706e.js');
+
+}
+/*
+     FILE ARCHIVED ON 05:35:17 Dec 28, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 02:55:52 Apr 13, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.688
+  exclusion.robots: 0.024
+  exclusion.robots.policy: 0.011
+  esindex: 0.018
+  cdx.remote: 11.358
+  LoadShardBlock: 154.941 (3)
+  PetaboxLoader3.datanode: 737.725 (4)
+  PetaboxLoader3.resolve: 2220.052 (3)
+  load_resource: 2814.138
+*/
