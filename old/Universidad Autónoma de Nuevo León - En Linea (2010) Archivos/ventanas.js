95b7a685a66047e0d3025d4910a22ef627e88bde
Eloyaso2008 <eloyaso2008@gmail.com>
2025-06-13 19:19:16 -0600

    Add files via upload

---
+++ old/Universidad Autónoma de Nuevo León - En Linea (2010) Archivos/ventanas.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function abre_ventana(url, nombre, parametros){
+	window.open (url, nombre, parametros);
+}
+
+function nva_ventana(url){
+	window.location=url;
+}
+
+}
+/*
+     FILE ARCHIVED ON 08:39:19 Sep 21, 2010 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:41:18 Apr 13, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.621
+  exclusion.robots: 0.02
+  exclusion.robots.policy: 0.009
+  esindex: 0.01
+  cdx.remote: 12.853
+  LoadShardBlock: 234.77 (3)
+  PetaboxLoader3.datanode: 195.085 (4)
+  PetaboxLoader3.resolve: 196.361 (2)
+  load_resource: 169.444
+*/
